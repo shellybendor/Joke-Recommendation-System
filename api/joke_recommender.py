416784deafd9b1148da6d8ee322c3d9e71499bb1
shelly.bendor <shelly.bendor@cs.huji.ac.il>
2022-04-05 18:33:08 +0300

    fixed some bugs

--- api/joke_recommender.py
+++ api/joke_recommender.py
@@ -1,12 +1,11 @@
 import numpy as np 
 import pandas as pd
-from pathlib import Path
 import random
 from matrix_factorization import KernelMF, train_update_test_split
 from sklearn.metrics import mean_squared_error
 
 
-class DatabaseWrapper:
+class JokeRecommender:
     NOT_RATED = 99
 
     def __init__(self):
@@ -15,7 +14,7 @@ class DatabaseWrapper:
         self._jokes_df = pd.read_csv("all_jokes.csv")
         self._current_max_user_index = self._user_item_df.shape[0] - 1
         self._num_jokes = self._jokes_df.shape[0]
-        self._mf = KernelMF(n_epochs=20, n_factors=20, verbose=0, lr=0.001, reg=0.005, min_rating=-10, max_rating=10)
+        self._mf = KernelMF(n_epochs=20, n_factors=50, verbose=0, lr=0.001, reg=0.005, min_rating=-10, max_rating=10)
         self._ratings = self._preprocess_data()
         self._mf.fit(self._ratings[["user_id", "item_id"]], self._ratings["rating"])
     
@@ -51,18 +50,17 @@ class DatabaseWrapper:
         user_ind = self._get_user_index(user_name)
         if self._user_item_df[f"joke_{joke_num}"][user_ind] != self.NOT_RATED:
             raise Exception(f"Joke {joke_num} has already been rated by user with id: {user_ind}")
-        self._user_item_df.loc[f"joke_{joke_num}", user_ind] = rating
-        self._user_item_df.loc["num_ratings", user_ind] = self._user_item_df["num_ratings"][user_ind] + 1
+        self._user_item_df.loc[user_ind, f"joke_{joke_num}"] = rating
+        self._user_item_df.loc[user_ind, "num_ratings"] = self._user_item_df["num_ratings"][user_ind] + 1
         self._ratings.loc[len(self._ratings)] = np.array([user_ind, f"joke_{joke_num}", rating])
         ratings_for_update = self._ratings[self._ratings["user_id"].astype(int) == user_ind]
-        # ratings_for_update = self._ratings.query("user_id == @user_ind")
         self._mf.update_users(
             ratings_for_update[["user_id", "item_id"]], ratings_for_update["rating"].astype(np.float32), lr=0.001, n_epochs=20, verbose=0
         )
 
     def save_changes_to_db(self):
-        self._user_item_df.to_csv("user_item_matrix.csv")
-        self._users_df.to_csv("all_users.csv")
+        self._user_item_df.to_csv("user_item_matrix.csv", index=False)
+        self._users_df.to_csv("all_users.csv", index=False)
 
     def _preprocess_data(self):
         user_items_ratings_df = self._user_item_df.drop('num_ratings', axis=1)
@@ -71,7 +69,8 @@ class DatabaseWrapper:
         return user_items_ratings_df
 
     def evaluate_model(self):
-        data_df = self._preprocess_data()
+        counts = self._ratings["user_id"].value_counts()
+        eval_data = self._ratings[self._ratings.user_id.isin(counts.index[counts.gt(1)])]
         # Splitting data into existing user ratings for training, new user's ratings for training, and new user's ratings for testing.
         (
             X_train_initial,
@@ -80,10 +79,10 @@ class DatabaseWrapper:
             y_train_update,
             X_test_update,
             y_test_update,
-        ) = train_update_test_split(data_df, frac_new_users=0.2)
+        ) = train_update_test_split(eval_data, frac_new_users=0.2)
 
         # Initial training
-        matrix_fact = KernelMF(n_epochs=20, n_factors=20, verbose=1, lr=0.001, reg=0.005, min_rating=-10, max_rating=10)
+        matrix_fact = KernelMF(n_epochs=50, n_factors=50, verbose=1, lr=0.001, reg=0.005, min_rating=-10, max_rating=10)
         matrix_fact.fit(X_train_initial, y_train_initial)
 
         # Update model with new users
@@ -94,7 +93,9 @@ class DatabaseWrapper:
         # Test model on predictions for "new" users
         pred = matrix_fact.predict(X_test_update)
         rmse = mean_squared_error(y_test_update, pred, squared=False)
-        print(f"\nTest RMSE: {rmse:.4f}")        
+        norm_rmse = rmse / 20
+        print(f"\nTest RMSE: {rmse:.4f}")
+        print(f"\nTest Normalized RMSE: {norm_rmse:.4f}")
 
     def _get_recommended_joke(self, user: str):
         items_known = self._ratings.query("user_id == @user")["item_id"]
@@ -105,10 +106,10 @@ class DatabaseWrapper:
     def get_joke(self, user_name: str):
         user = self._get_user_index(user_name)
         if self._get_num_jokes_rated(user) < 10:
-            print("Getting random joke")
+            print("Here is a random joke:\n")
             return self._get_random_new_joke(user)
         else:
-            print("Getting recommened joke")
+            print("Here is a recommened joke:\n")
             return self._get_recommended_joke(user_name)
 
     def _get_num_jokes_rated(self, user: int):
@@ -116,13 +117,20 @@ class DatabaseWrapper:
 
 
 
-tmp = DatabaseWrapper()
-# tmp.evaluate_model()
-# import ipdb; ipdb.set_trace()
-tmp.add_new_user("shelly")
-while True:
-    num, joke = tmp.get_joke("shelly")
-    print(joke)
-    rating = float(input("how was the joke?"))
-    # import ipdb; ipdb.set_trace()
-    tmp._add_joke_rating("shelly", num, rating)
+recommender = JokeRecommender()
+username = input("Welcome to the joke recommender! What is your name? \n")
+recommender.add_new_user(username)
+print(f"Welcome {username}!")
+keep_going = True
+while keep_going:
+    num, joke = recommender.get_joke(username)
+    print(joke, end="\n\n")
+    rating = float(input("How would you rate the joke from -10.0 to 10? "))
+    recommender._add_joke_rating(username, num, rating)
+    answer = input("Want another joke? y/n ")
+    keep_going = (answer == "y")
+print("Thanks for taking part. Please wait while we save everything :)")
+recommender.save_changes_to_db()
+answer = input("Evaluate Model? y/n ")
+if (answer == "y"):
+    recommender.evaluate_model()
